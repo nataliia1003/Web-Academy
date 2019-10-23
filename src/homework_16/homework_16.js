bafccd8a5f2bae877bdd277e0caf9772de8888cb
Nataliia Antoniuk <natasha_kosa@ukr.net>
2019-10-24 01:21:33 +0300

    homeworck_16

---
+++ src/homework_16/homework_16.js
@@ -0,0 +1,20 @@
+import './homework_16.scss';
+
+import { clock } from './clock';
+
+function myAnalitic() {
+    let clickCounts = 0;
+    const buttons = document.querySelectorAll('button');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            clickCounts++;
+            console.log(clickCounts);
+        });
+    }
+}
+
+clock(undefined, 'SHORT');
+clock();
+clock(undefined, undefined,'DATE');
+myAnalitic();
